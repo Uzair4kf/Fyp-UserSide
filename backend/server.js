e5fca8d4d974644ad638e6e172e3ff42b382d776
Uzair4kf <limiaraion@gmail.com>
2022-04-26 07:29:57 +0500

    c

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 const app = express();
 dotenv.config();
-connectDb();
+connectDB();
+const PORT = process.env.PORT || 5002;
+app.listen(
+  PORT,
+  console.log(`Server running on ${process.env.NODE_ENV}mode on port ${PORT}`)
+);
+app.get("/products", (req, res) => {
+  res.json(products);
+});
