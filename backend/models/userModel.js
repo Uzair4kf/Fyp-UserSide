9a71f9ea8f82288ef34c3d44bff783592ddca8a0
Uzair4kf <limiaraion@gmail.com>
2022-05-15 17:28:53 +0500

    c

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,13 +2,25 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    googleId: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
       unique: true,
     },
 
-    password: { 
+    password: {
       type: String,
       required: true,
     },
--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,21 @@ import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import User from "./models/userModel.js";
 import session from "express-session";
-import passport from "./config/passport.js";
+
 const app = express();
 dotenv.config();
 connectDB();
 app.use(cors());
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
-  })
-);
-app.use(passport.initialize);
-app.use(passport.session);
+
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true },
+//   })
+// );
+
 const PORT = process.env.PORT || 5002;
 app.listen(
   PORT,
