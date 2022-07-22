3e9ea9120035c6cfecfe64cb92128411fb890ca2
Uzair4kf <limiaraion@gmail.com>
2022-07-22 23:24:46 +0500

    c

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
-  );
+  );   
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With, Content-Type, Authorization"
